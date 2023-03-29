9faae7b7b63574fde8bc8811727a8e19a367abae
Esrael Asefa <esraelasefa823@gmail.com>
2023-03-29 16:06:30 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 var bodyParser = require("body-parser");
+const port = process.env.PORT || 8080;
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,6 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-var listener = app.listen(8080, function () {
+var listener = app.listen(port, function () {
   console.log("Listening on port " + listener.address().port);
 });
